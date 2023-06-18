c2c364309d7b8b71e18ec47d4e3224169871929f
Ayah Harper <136263768+iharpay@users.noreply.github.com>
2023-06-18 16:36:45 -0400

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,20 @@
+/* parser.h
+*
+* Ayah Harper
+* 2/24/2023
+*
+* Purpose: Declares the parseRString function, 
+which accepts input of type istream and returns a 
+string in rstring format.
+*
+*/
+
+#ifndef _PARSER_
+#define _PARSER_
+
+#include <iostream>
+#include <string>
+
+std::string parseRString(std::istream &input);
+
+#endif
